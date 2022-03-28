ef933ecf93c52824493346c9d8e3e46713f588a6
Polyphemus <beans4258@gmail.com>
2022-03-28 00:20:47 -0400

    problem completed

--- _16_3sum_closest.py
+++ _16_3sum_closest.py
@@ -6,77 +6,56 @@
 
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        # brute force with some optimization, too slow
-        # order doesn't matter so trie doesn't make sense
-        # because unique combos don't matter, we'll skip all nums. before parent as those have been checked already
-        # if we sort, and largest three are smaller than target, return largest three. If smaller three are larger than target, return those
-        # if three largest are closest to target, iterate in reverse
+        # basically brute force with optimization
         
-        nums.sort()
-        print(nums)
+        nums.sort()  # O(n log n) time
+
+        # Optimization: if largest three are smaller than target, return largest three. If smaller three are larger than target, return those
         if nums[0] + nums[1] + nums[2] >= target:
             return nums[0] + nums[1] + nums[2]
         
         if nums[len(nums) - 1] + nums[len(nums) - 2] + nums[len(nums) - 3] <= target:
             return nums[len(nums) - 1] + nums[len(nums) - 2] + nums[len(nums) - 3]
-        
-        # if the first three combined are closer to target than the last three, iterate from the beginning
+
+        # Because uniquely ordered combos don't matter, we'll skip all nums before parent pointer as those have been checked already
+        # i.e. no point in checking 1+3+2 if we've already checked 1+2+3
+
+        # Optimization: if the first three combined are closer to target than the last three, iterate from the beginning
         if abs(target - (nums[0] + nums[1] + nums[2])) < abs(nums[len(nums) - 1] + nums[len(nums) - 2] + nums[len(nums) - 3]):
             diff = target - (nums[0] + nums[1] + nums[2])
-    #        print(f'initial diff = {diff}')
 
             for i in range(len(nums)):
-    #            print(f'\ni = {i}')
-                for j in range(i+1, len(nums)):                        
-    #                print(f'j = {j}')
+                for j in range(i+1, len(nums)):
                     for k in range(j+1, len(nums)):
-                        if target - (nums[i] + nums[j] + nums[k]) == 0:
-    #                            print(f'Exact match! i = {i}, j = {j}, k = {k}')
+                        if target - (nums[i] + nums[j] + nums[k]) == 0:  # Optimization: if target is hit, return target
                             return nums[i] + nums[j] + nums[k]
 
-                        if abs(target - (nums[i] + nums[j] + nums[k])) < abs(diff): # if current sum is closer to target
+                        if abs(target - (nums[i] + nums[j] + nums[k])) < abs(diff):  # if current sum is closer to target, save new difference
                             diff = target - (nums[i] + nums[j] + nums[k])
-    #                        print(f'new diff = {diff}')
 
+                        # If current sum is further from target than previous nums[k] sum, break because it won't improve moving further along number line
                         elif k != j + 1 and abs(target - (nums[i] + nums[j] + nums[k])) > abs(target - (nums[i] + nums[j] + nums[k - 1])):
-                            # break if current sum is further from target than previous nums[k] sum because it won't improve moving further along number line
-    #                        print(f'nums[k-1] = {nums[k-1]}')
-    #                        print(f'nums[k] = {nums[k]}')
-    #                        print('break')
                             break
-
-                        else:    
-    #                        print('passing')
+                        else:
                             pass
+
         # if the last three combined are closer to target than the last three, iterate from the end
         else:
             diff = target - (nums[len(nums) - 1] + nums[len(nums) - 2] + nums[len(nums) - 3])
-#            print(f'initial diff = {diff}')
             for i in range(len(nums)-1, -1, -1):
-#                print(f'\ni = {i}')
-                for j in range(i-1, -1, -1):                        
-#                    print(f'j = {j}')
+                for j in range(i-1, -1, -1):
                     for k in range(j-1, -1, -1):
-#                        print(f'k = {k}')
-#                        print(f'current sum = {nums[i] + nums[j] + nums[k]}')
+
                         if target - (nums[i] + nums[j] + nums[k]) == 0:
-#                            print(f'Exact match! i = {i}, j = {j}, k = {k}')
                             return nums[i] + nums[j] + nums[k]
 
-                        if abs(target - (nums[i] + nums[j] + nums[k])) < abs(diff): # if current sum is closer to target
+                        if abs(target - (nums[i] + nums[j] + nums[k])) < abs(diff):
                             diff = target - (nums[i] + nums[j] + nums[k])
-#                            print(f'new diff = {diff}')
 
                         elif k!= j - 1 and abs(target - (nums[i] + nums[j] + nums[k])) > abs(target - (nums[i] + nums[j] + nums[k + 1])):
-                            # break if current sum is further from target than previous nums[k] sum because it won't improve moving further along number line
-    #                        print(f'nums[k-1] = {nums[k-1]}')
-    #                        print(f'nums[k] = {nums[k]}')
-                            print('break')
                             break
 
                         else:    
-#                            print('passing')
                             pass
-            
-        
+
         return target - diff      
